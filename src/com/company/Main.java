4c5875376350409470be1b1fb561ffbb4bc27ec9
Zigmas <zambrazaitis@gmail.com>
2018-01-30 16:07:11 +0200

    uzduotis11 finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // 1.10​. Sukurti skaičiuotuvo klasę, kurioje realizuoti statinius metodus sumai, skirtumui,
+        // sandaugai. Per parametrus bus naudojami sveiko tipo skaičiai.
+        // 1.11.​ Papildyti 1.10.​ programą, panaudojant metodų perkrovimą (overloading), sukurti
+        // papildomus metodus, kad operacijos veiktų tiek su sveiko tipo skaičiais, tiek su skaičiais
+        // po kablelio.
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite pirmaji skaiciu: ");
+        float sk1 = sc.nextFloat();
+        System.out.println("Iveskite antraji skaiciu: ");
+        float sk2 = sc.nextFloat();
+        Veiksmai.suma(sk1, sk2);
+        Veiksmai.skirtumas(sk1, sk2);
+        Veiksmai.sandauga(sk1, sk2);
     }
 }
---
+++ src/com/company/Veiksmai.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Veiksmai {
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    public static int skirtumas(int a, int b) {
+        return a - b;
+    }
+
+    public static int sandauga(int a, int b) {
+        return a * b;
+    }
+    public static void suma(float a, float b) {
+        float suma = a + b;
+        System.out.println("Suma= " + suma);
+    }
+    public static void skirtumas(float a, float b) {
+        float skirtumas = a - b;
+        System.out.println("Skirtumas= " + skirtumas);
+    }
+    public static void sandauga(float a, float b) {
+        float sandauga = a*b;
+        System.out.println("Sandauga= " + sandauga);
+    }
+}
